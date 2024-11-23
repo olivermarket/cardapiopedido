ea0293884e52f31a9c32515b5c87d22c5e998c60
olivermarket <olivermarketindgi@gmail.com>
2024-11-23 15:07:19 -0300

    subir arquivos alerado

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 09 && hora < 23; 
+  return hora >= 10 && hora < 22; 
   //true = restaurante está aberto 
 }
 
